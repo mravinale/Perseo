0170105897b9c2c20d86f33d3e456ba1b7514a57
Mariano Ravinale <mravinale@gmail.com>
2013-02-28 22:10:56 -0300

    fixed problem with widget ko binding

--- Excalibur.Web/App/durandal/widgets/parallax/controller.js
+++ Excalibur.Web/App/durandal/widgets/parallax/controller.js
@@ -1,12 +1,18 @@
-﻿define(function (require) {
-    var widget = require('durandal/widget');
+﻿define(function (require) { 
+    var system = require('durandal/system');
 
     var ctor = function (element, settings) {
-        this.settings = settings; 
+        this.settings = settings;
     };
     
     ctor.prototype.viewAttached = function (view) {
-        $(view).children().cslider(this.settings.options);
+        $(view).cslider(this.settings.options);
+        
+        system.log('View Attached', view);
+    };
+    
+    ctor.prototype.afterRenderItem = function (elements, item) {
+        system.log('afterRenderItem', item);
     };
 
     return ctor;
--- Excalibur.Web/App/viewmodels/welcome.js
+++ Excalibur.Web/App/viewmodels/welcome.js
@@ -1,8 +1,18 @@
 ﻿define(function (require) {
-    var system = require('durandal/system'),
-          app = require('durandal/app');
+    var system = require('durandal/system'), 
+    widget = require('durandal/widget');
+
+    widget.registerKind('parallax'); 
 
     var ctor = function (name, description) {
+        this.slides = ko.observableArray(["Title1", "Title2", "Title3", "Title4"]);
+    };
+    
+    ctor.prototype.viewAttached = function (view) {
+        //$('p').editable({
+        //    url: '/post',
+        //    title: 'Enter comments'
+        //});
     };
 
     ctor.prototype.activate = function () {

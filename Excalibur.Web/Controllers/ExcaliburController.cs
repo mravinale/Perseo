9265596e53f83453f4c24fd90965f2ee260ea6b5
Mariano Ravinale <mravinale@gmail.com>
2013-03-11 00:20:28 -0300

    added mising file

---
+++ Excalibur.Web/Controllers/ExcaliburController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace Excalibur.Web.Controllers 
+{
+	public class ExcaliburController : Controller 
+	{
+		public ActionResult Index() 
+		{
+			return View();
+		}
+	}
+}
